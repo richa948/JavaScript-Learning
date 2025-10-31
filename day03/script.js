53fd4519b67efc83221f721d1f2aa4ec0f48e462
richa948 <padmasuman55@gmail.com>
2025-10-31 17:53:59 +0530

    Update script.js

--- day03/script.js
+++ day03/script.js
@@ -1,3 +1,8 @@
 let fullName = prompt("Enter your fullName without spaces");
 let username ="@" + fullName + fullName.length;
 console.log(username);
+
+let companies = ["Meta", "Apple", "Amazon", "Netflix", "Google"];
+// companies.shift();
+// companies.splice(2, 1, "IBM");
+companies.push("Ola");
