c9910595503f7b1a619bf5888ce5905fc9cb97a7
richa948 <padmasuman55@gmail.com>
2025-11-03 17:12:03 +0530

    Create    script.js

---
+++ day04/script.js
@@ -0,0 +1,24 @@
+// function countVowels(str){
+//     //Apnacollege , cnt = 5
+//      let cnt = 0;
+//     for(const char of str){
+//         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//             cnt++;
+//         }
+//     }
+//     console.log(cnt);
+// }
+
+//  **For each loop->
+
+// let arr = ["pune" , "Hyderabad", "bangalore"];
+// arr.forEach((val , Idx, arr) => {
+//     console.log(val.toUpperCase(), Idx, arr);
+// });
+
+ //practice qs ->
+let nums = [2, 3, 4, 5, 6];
+
+nums.forEach((num) => {
+    console.log(num*num);
+});
