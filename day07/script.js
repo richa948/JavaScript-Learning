753133da1183e3b15e21f89edbe528ba7fb5f6a0
richa948 <padmasuman55@gmail.com>
2025-11-06 11:25:49 +0530

    Add files via upload

---
+++ day07/script.js
@@ -0,0 +1,10 @@
+let button = document.createElement("but");
+button.innerText = "Click me!";
+
+button.style.backgroundColor = "black";
+button.style.color = "white";
+
+document.querySelector("body").prepend(button);
+
+//Q-2
+let para = document.querySelector("p");
